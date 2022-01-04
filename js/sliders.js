1d419c40dedf2fd7de8b8f66c155609df938cbde
atom <vlad.v428@gmail.com>
2022-01-05 05:20:33 +0600

    commit

---
+++ js/sliders.js
@@ -0,0 +1,49 @@
+window._ = new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1, //'auto',
+    slidesToScroll: 1,
+    itemWidth: 268,
+    draggable: true,
+    scrollLock: false,
+    rewind: true,
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+    },
+    responsive: [
+        {
+            breakpoint: 800,
+            settings: {
+                slidesToScroll: 'auto',
+                itemWidth: 300,
+                slidesToShow: 'auto',
+                exactWidth: true
+            }
+        },
+        {
+            breakpoint: 700,
+            settings: {
+                slidesToScroll: 4,
+                slidesToShow: 4,
+                dots: false,
+                arrows: false,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToScroll: 3,
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToScroll: 2,
+                slidesToShow: 2,
+                dots: false,
+                arrows: false,
+                scrollLock: true
+            }
+        }
+    ]
+});
