84d2f48ca2598e0a2af72262a1eef487e3720ec1
atom <vlad.v428@gmail.com>
2022-01-18 08:09:10 +0600

    23

---
+++ js/burger.js
@@ -0,0 +1,7 @@
+function burger_add() {
+    document.querySelector(".mobile_menu").classList.add('mobile_menu_active');
+}
+
+function burger_rem() {
+    document.querySelector(".mobile_menu").classList.remove('mobile_menu_active')
+}
